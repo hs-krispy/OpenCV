876d30d728871100b6764f97e5975b50d61ea5ea
hs-krispy <58063806+hs-krispy@users.noreply.github.com>
2020-11-16 11:57:00 +0900

    Update canny edge annotation

--- Gradients and edge detection/canny.py
+++ Gradients and edge detection/canny.py
@@ -9,6 +9,6 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image = cv2.GaussianBlur(image, (5, 5), 0)
 cv2.imshow("Blurred", image)
 canny = cv2.Canny(image, 30, 150) # blur 이미지(single channel), 임계값1, 임계값2
-# 변화값이 임계값1보다 작은 것은 edge X, 변화값이 임계값2보다 큰 값은 edge, 임계값 사이의 값들은
+# 변화값이 임계값1보다 작은 것은 edge X, 변화값이 임계값2보다 큰 값은 강한 edge, 임계값 사이의 값들은 강한 edge와 연관성을 판별하고 연관성이 있는 경우에만 edge로 추가
 cv2.imshow("Canny", canny)
 cv2.waitKey(0)
